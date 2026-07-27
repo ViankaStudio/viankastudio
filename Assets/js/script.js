f94216895f9a0a75513dafe9d5a6d41e8d0944fb
Vianka Studio <viankastudio@gmail.com>
2026-07-28 02:25:48 +0700

    Add interactive FAQ section

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,13 @@
-console.log("Website Vianka Studio berhasil dijalankan!");
+console.log("Website Vianka Studio berhasil dijalankan!");const faqItems = document.querySelectorAll(".faq-item");
+
+faqItems.forEach(item => {
+
+    const button = item.querySelector(".faq-question");
+
+    button.addEventListener("click", () => {
+
+        item.classList.toggle("active");
+
+    });
+
+});
